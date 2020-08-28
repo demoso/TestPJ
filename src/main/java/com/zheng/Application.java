10c926c594b894dfa5d81e82f02054539bb9e2df
Administrator <admin@example.com>
2020-08-28 22:33:40 +0800

    1

--- src/main/java/com/zheng/Application.java
+++ src/main/java/com/zheng/Application.java
@@ -21,7 +21,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableMethodCache(basePackages = "com.zheng")
 @EnableCreateCacheAnnotation
 @EnableBinding(Source.class)
-//@EnableDistributedLock
+@EnableDistributedLock
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
